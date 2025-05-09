3199aa6f35683e85517966899c8a9eff1b34934a
alina <komo2000@yandex.ru>
2025-05-10 00:06:28 +0300

    First commit.

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,53 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	s := strings.Split(datastring, ",")
+	if len(s) != 2 {
+		return errors.New("error in function Parse")
+	}
+	steps, err := strconv.Atoi(s[0])
+	if err != nil {
+		return err
+	}
+	ds.Steps = steps
+	if ds.Steps <= 0 {
+		return err
+	}
+	duration, err := time.ParseDuration(s[1])
+	if err != nil {
+		return err
+	}
+	ds.Duration = duration
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	spentCalories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return " ", err
+	}
+	result := fmt.Sprintf("Количество шагов: %d \nДистанция составила %.2f км.\nВы сожгли %.2f \n", ds.Steps, distance, spentCalories)
+	return result, nil
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,71 @@
 package trainings
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type Training struct {
 	// TODO: добавить поля
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
 }
 
 func (t *Training) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	s := strings.Split(datastring, ",")
+	if len(s) != 3 {
+		return errors.New("error in function Parse")
+	}
+	steps, err := strconv.Atoi(s[0])
+	if err != nil {
+		return err
+	}
+	t.Steps = steps
+	if t.Steps <= 0 {
+		return err
+	}
+	t.TrainingType = s[1]
+
+	duration, err := time.ParseDuration(s[2])
+	if err != nil {
+		return err
+	}
+	t.Duration = duration
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	var spentCalories float64
+	var err error
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	meanSpeed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+	duration := t.Duration.Hours()
+	training := t.TrainingType
+	if training == "Бег" {
+		spentCalories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			return " ", err
+		}
+	} else if training == "Ходьба" {
+		spentCalories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+		if err != nil {
+			return " ", err
+		}
+	} else {
+		return " ", errors.New("error in function Parse")
+	}
+	result := fmt.Sprintf("Тип тренировки: %s \nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f \n", training, duration, distance, meanSpeed, spentCalories)
+
+	return result, nil
+
 }
